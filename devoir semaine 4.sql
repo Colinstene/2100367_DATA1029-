a62c385bf13c0038fd67d7609029be6b13f60f37
Colinstene <96065384+Colinstene@users.noreply.github.com>
2024-02-12 12:48:34 -0400

    question 3

--- devoir semaine 4.sql
+++ devoir semaine 4.sql
@@ -38,3 +38,13 @@ WHERE au_id IN (
         WHERE ta2.au_id = ta.au_id
     )
 );
+-- Question 3 Obtenir la liste des noms d'auteurs ayant touche une avance superieures a toutes les avances versees par l'editeur "Harmattan"
+
+SELECT au_fname, au_lname
+FROM authors
+INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
+INNER JOIN titles ON titleauthor.title_id = titles.title_id
+INNER JOIN publishers ON titles.pub_id = publishers.pub_id
+WHERE publishers.pub_name = 'Harmattan'
+GROUP BY authors.au_id
+HAVING MAX(titles.advance) > (SELECT MAX(advance) FROM titles INNER JOIN publishers ON titles.pub_id = publishers.pub_id WHERE publishers.pub_name = 'Harmattan');

3298eb4881e14d59302816430844f7011a2f9a7c
Colinstene <96065384+Colinstene@users.noreply.github.com>
2024-02-21 09:39:36 -0400

    test commit

--- devoir semaine 4.sql
+++ devoir semaine 4.sql
@@ -1,4 +1,5 @@
 -- Question 1 Obtenir la liste des auteurs dont l’éditeur « Harmattan » n’a publié aucun livre :
+-- solution a la suite 
 SELECT au_fname, au_lname
 FROM authors
 WHERE au_id NOT IN (
